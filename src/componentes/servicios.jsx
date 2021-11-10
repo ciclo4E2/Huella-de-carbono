12a467764d7d23d78d38621f427d36e495a0aee9
YuderHernandez <yuderdavid@gmail.com>
2021-11-09 21:26:59 -0500

    se ingresa menu navegacion

---
+++ src/componentes/servicios.jsx
@@ -0,0 +1,33 @@
+import logo from '../logo.svg';
+import '../App.css';
+
+function Servicios() {
+    return (
+      <div className="Integrantes">
+          <div>
+          <img src={logo} className="App-logo" alt="logo" />
+        <p>servico 1</p>
+
+          </div>
+        
+          <div>
+          <img src={logo} className="App-logo" alt="logo" />
+        <p>servicio 2</p>
+
+          </div>
+
+          <div>
+          <img src={logo} className="App-logo" alt="logo" />
+        <p>servico 3</p>
+
+          </div>
+
+
+          
+      </div>
+    );
+  }
+
+
+
+export default Servicios;
