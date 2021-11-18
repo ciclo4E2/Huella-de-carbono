09cdbaf86cb237aa5cbbd4ace29e4710bd7c1af1
yesmore7 <89815425+yesmore7@users.noreply.github.com>
2021-11-18 14:12:52 -0500

    Actualizacion noticias

--- src/componentes/noticias.jsx
+++ src/componentes/noticias.jsx
@@ -7,26 +7,30 @@ function Noticias() {
           <div>
           <img src={logo} className="App-logo" alt="logo" />
         <p>Noticia 1</p>
-<a href="https://www.un.org/es/climatechange/cop26">Mayor Información</a>
+<a href="https://www.un.org/es/climatechange/cop26">Glasgow 2021</a>
+<p>Cambio Climático</p>
           </div>
         
           <div>
           <img src={logo} className="App-logo" alt="logo" />
         <p>Noticia 2</p>
-
+        <a href="https://news.un.org/es/story/2020/12/1485312">CO2 Emisiones</a>
+<p>Record</p>
           </div>
 
           <div>
           <img src={logo} className="App-logo" alt="logo" />
         <p>Noticia 3</p>
-
+        <a href="https://medlineplus.gov/spanish/pruebas-de-laboratorio/dioxido-de-carbono-co2-en-la-sangre/">Efectos Salud</a>
+<p>Consecuencias</p>
           </div>
 
 
           <div>
           <img src={logo} className="App-logo" alt="logo" />
         <p>Noticia 4</p>
-
+        <a href="https://www.wwf.org.co/?365023/Guia-para-entender-los-aportes-de-Colombia-a-la-lucha-contra-el-cambio-climatico">Compromiso Colombia</a>
+<p>Aportes y Medidas</p>
           </div>
 
 
