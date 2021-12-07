7e47a98620177a733735b6cbc86d710b42775d0f
YuderHernandez <yuderdavid@gmail.com>
2021-12-07 00:16:13 -0500

    se incluyen fotos integrantes

--- src/componentes/integrantes.jsx
+++ src/componentes/integrantes.jsx
@@ -1,28 +1,31 @@
 
 import logo1 from '../imagenes/images.jpg';
+import melanieFoto from '../imagenes/melanie.png';
+import yesidFoto from '../imagenes/yesid.png';
+import yuderFoto from '../imagenes/yuder.png';
 import '../App.css';
 
 function Integrantes() {
     return (
         <div className="componente">
           <div>
-          <img src={logo1} className="App-logo" alt="logo" />
+          <img src={melanieFoto} className="App-logo" alt="logo" />
         <p>Melanie</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, quas?</p>
+        <p>Ingeniera ambiental, por un gran gusto por la tecnologia</p>
 
           </div>
         
           <div>
-          <img src={logo1} className="App-logo" alt="logo" />
+          <img src={yesidFoto} className="App-logo" alt="logo" />
         <p>Yesid</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, quas?</p>
+        <p>Administrador de empresas, siempre dispuesto a colaborar con la mejor actitud</p>
 
           </div>
 
           <div>
-          <img src={logo1} className="App-logo" alt="logo" />
+          <img src={yuderFoto} className="App-logo" alt="logo" />
         <p>Yuder</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, quas?</p>
+        <p>Estudiante de ing mecatronica, entregado a su labor. Me dedico a ser mto industrial</p>
 
           </div>
 
